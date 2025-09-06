3f1758bbb4e153fea7379541b9da105fbead5490
DRK-V <flv3dsc@gmail.com>
2025-09-05 19:51:19 -0500

    ajustes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; 
-import { BrowserRouter } from "react-router-dom"; 
-import App from "./App"; 
-import "./index.css"; 
-ReactDOM.createRoot(document.getElementById("root")).render(
-<BrowserRouter><App />
-</BrowserRouter>
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+
+// Le decimos a TypeScript que document.getElementById("root") nunca será null
+const rootElement = document.getElementById("root")!;
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
